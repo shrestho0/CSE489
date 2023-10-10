9661cb61bad573fc8c33370cd30e1488dc89dee0
Sadril Amin Shrestho <sadrilaminshrestho@gmail.com>
2023-10-11 01:24:18 +0600

    whatever

--- Practices/_t05_mvvm_partial/lib/constants.dart
+++ Practices/_t05_mvvm_partial/lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static String appTitle = "Partial MVVM";
+  static String appTitle = "Partial MVVM :3";
 }
 
 const String USER_LIST = 'https://jsonplaceholder.typicode.com/users';
